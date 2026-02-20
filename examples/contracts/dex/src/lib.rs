5f3850dda673bd4e42daa726a20fc815ae63aed2
Dev Jaja <jajak5242@gmail.com>
2026-02-20 14:44:56 -0500

    fix: fixes the CI failing errors

--- examples/contracts/dex/src/lib.rs
+++ examples/contracts/dex/src/lib.rs
@@ -14,24 +14,56 @@ pub struct DexContract;
 #[contractimpl]
 impl DexContract {
     pub fn add_liquidity(env: Env, amount_a: i128, amount_b: i128) {
-        let reserve_a: i128 = env.storage().instance().get(&DataKey::ReserveA).unwrap_or(0);
-        let reserve_b: i128 = env.storage().instance().get(&DataKey::ReserveB).unwrap_or(0);
+        let reserve_a: i128 = env
+            .storage()
+            .instance()
+            .get(&DataKey::ReserveA)
+            .unwrap_or(0);
+        let reserve_b: i128 = env
+            .storage()
+            .instance()
+            .get(&DataKey::ReserveB)
+            .unwrap_or(0);
 
-        env.storage().instance().set(&DataKey::ReserveA, &(reserve_a + amount_a));
-        env.storage().instance().set(&DataKey::ReserveB, &(reserve_b + amount_b));
+        env.storage()
+            .instance()
+            .set(&DataKey::ReserveA, &(reserve_a + amount_a));
+        env.storage()
+            .instance()
+            .set(&DataKey::ReserveB, &(reserve_b + amount_b));
     }
 
     pub fn remove_liquidity(env: Env, amount_a: i128, amount_b: i128) {
-        let reserve_a: i128 = env.storage().instance().get(&DataKey::ReserveA).unwrap_or(0);
-        let reserve_b: i128 = env.storage().instance().get(&DataKey::ReserveB).unwrap_or(0);
+        let reserve_a: i128 = env
+            .storage()
+            .instance()
+            .get(&DataKey::ReserveA)
+            .unwrap_or(0);
+        let reserve_b: i128 = env
+            .storage()
+            .instance()
+            .get(&DataKey::ReserveB)
+            .unwrap_or(0);
 
-        env.storage().instance().set(&DataKey::ReserveA, &(reserve_a - amount_a));
-        env.storage().instance().set(&DataKey::ReserveB, &(reserve_b - amount_b));
+        env.storage()
+            .instance()
+            .set(&DataKey::ReserveA, &(reserve_a - amount_a));
+        env.storage()
+            .instance()
+            .set(&DataKey::ReserveB, &(reserve_b - amount_b));
     }
 
     pub fn swap(env: Env, token_in: bool, amount_in: i128) -> i128 {
-        let reserve_a: i128 = env.storage().instance().get(&DataKey::ReserveA).unwrap_or(0);
-        let reserve_b: i128 = env.storage().instance().get(&DataKey::ReserveB).unwrap_or(0);
+        let reserve_a: i128 = env
+            .storage()
+            .instance()
+            .get(&DataKey::ReserveA)
+            .unwrap_or(0);
+        let reserve_b: i128 = env
+            .storage()
+            .instance()
+            .get(&DataKey::ReserveB)
+            .unwrap_or(0);
 
         let (reserve_in, reserve_out) = if token_in {
             (reserve_a, reserve_b)
@@ -42,19 +74,35 @@ impl DexContract {
         let amount_out = (reserve_out * amount_in) / (reserve_in + amount_in);
 
         if token_in {
-            env.storage().instance().set(&DataKey::ReserveA, &(reserve_a + amount_in));
-            env.storage().instance().set(&DataKey::ReserveB, &(reserve_b - amount_out));
+            env.storage()
+                .instance()
+                .set(&DataKey::ReserveA, &(reserve_a + amount_in));
+            env.storage()
+                .instance()
+                .set(&DataKey::ReserveB, &(reserve_b - amount_out));
         } else {
-            env.storage().instance().set(&DataKey::ReserveA, &(reserve_a - amount_out));
-            env.storage().instance().set(&DataKey::ReserveB, &(reserve_b + amount_in));
+            env.storage()
+                .instance()
+                .set(&DataKey::ReserveA, &(reserve_a - amount_out));
+            env.storage()
+                .instance()
+                .set(&DataKey::ReserveB, &(reserve_b + amount_in));
         }
 
         amount_out
     }
 
     pub fn get_price(env: Env, token_in: bool) -> (i128, i128) {
-        let reserve_a: i128 = env.storage().instance().get(&DataKey::ReserveA).unwrap_or(0);
-        let reserve_b: i128 = env.storage().instance().get(&DataKey::ReserveB).unwrap_or(0);
+        let reserve_a: i128 = env
+            .storage()
+            .instance()
+            .get(&DataKey::ReserveA)
+            .unwrap_or(0);
+        let reserve_b: i128 = env
+            .storage()
+            .instance()
+            .get(&DataKey::ReserveB)
+            .unwrap_or(0);
 
         if token_in {
             (reserve_b, reserve_a)
@@ -72,7 +120,7 @@ mod test {
     #[test]
     fn test_liquidity_functions() {
         let env = Env::default();
-        let contract_id = env.register_contract(None, DexContract);
+        let contract_id = env.register(DexContract, ());
         let client = DexContractClient::new(&env, &contract_id);
 
         client.add_liquidity(&1000, &2000);
@@ -89,11 +137,11 @@ mod test {
     #[test]
     fn test_swap_function() {
         let env = Env::default();
-        let contract_id = env.register_contract(None, DexContract);
+        let contract_id = env.register(DexContract, ());
         let client = DexContractClient::new(&env, &contract_id);
 
         client.add_liquidity(&1000, &2000);
-        
+
         let amount_out = client.swap(&true, &100);
         assert_eq!(amount_out, 181);
 
@@ -102,4 +150,3 @@ mod test {
         assert_eq!(reserve_b, 1819);
     }
 }
-
